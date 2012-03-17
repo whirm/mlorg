8abf0b23f2ba6b92fe5f50c9e840e7cffcbe2fee
Simon Castellan <simon.castellan@iuwt.fr>
2012-03-17 11:41:16 +0100

    Added a start of an Inline module.

---
+++ syntax/inline/inline.ml
@@ -0,0 +1,92 @@
+
+open Prelude
+open Batteries
+(** {1 Type definition} *)
+type emphasis = [`Bold | `Italic | `Underline] * t list
+and entity = Entity.t
+and export_snippet = string * string
+and t = 
+  | Emphasis of emphasis 
+  | Entity of entity
+  | Export_Snippet of export_snippet
+  | Plain of string
+
+
+(** {1 Parsers} *)
+type parser = (string -> t list) -> Substring.t -> (t list * Substring.t) option
+let rec run_parsers parsers string =
+  let substring = Substring.all string in
+  let myself = run_parsers parsers in
+  let push acc chars = 
+    if chars = [] then acc
+    else
+      let s = unescape (Substring.all (String.of_list (List.rev chars))) in
+      Plain s :: acc
+  in
+  let rec aux chars acc substring = 
+    if Substring.is_empty substring then 
+      List.rev (push acc chars)
+    else
+      let rec try_parsers = function
+        | [] -> 
+          aux (Substring.get substring 0 :: chars)
+            acc
+            (Substring.triml 1 substring)
+        | t :: q -> try match t myself substring with
+            | None -> try_parsers q
+            | Some (r, cont) ->
+              aux [] (r @ push acc chars) cont
+          with _ -> try_parsers q
+      in try_parsers parsers
+  in aux [] [] substring
+
+(* The table of delimiters used to parse inline contents *)
+let delim_table = [('[', (']', false)); ('<', ('>', false));
+                   ('{', ('}', false));
+                   ('*', ('*', true)); ('_', ('_', true));
+                   ('~', ('~', true)); ('$', ('$', true)); 
+                   ('=', ('=', true)); ('/', ('/', true))]
+module D = Delimiters.Make (struct let table = delim_table end)
+open Substring
+module String = Substring
+
+(** {2 Emphasis} *)
+let emphasis_parser parse sub = 
+  let delims = ['*', `Bold; '_', `Underline; '/', `Italic] in
+  if List.mem_assoc sub.[0] delims then
+    match D.enclosing_delimiter sub sub.[0] with
+      | Some (s, rest) ->
+        Some ([Emphasis (List.assoc sub.[0] delims,
+                         parse s)], rest)
+      | None -> None
+  else None
+
+(** {2 Entity parser} *)
+let entity_parser _ s = 
+  if s.[0] = '\\' then
+    let name, rest = splitl 
+      (fun c -> Char.is_letter c || Char.is_digit c) (triml 1 s)
+    in
+    Some ([Entity (Entity.find (to_string name))], rest)
+  else
+    None
+
+(** {2 Export snippet parser} *)
+let export_snippet_parser _ s = 
+  if s.[0] = '@' then
+    let name, rest = splitl Char.is_letter (triml 1 s) in
+    (if rest.[0] = '{' then
+        (match D.enclosing_delimiter rest '{' with
+          | Some (s, rest) -> 
+            Some ([Export_Snippet (to_string name, unescape (all s))], rest)
+          | None -> None)
+      else
+        None)
+  else
+    None
+
+
+
+let parse = run_parsers
+  [emphasis_parser; entity_parser; export_snippet_parser]
+  
---
+++ syntax/inline/inline.mli
@@ -0,0 +1,84 @@
+(** Inline contents: type definition and output. *)
+
+
+(** {1 Description} *)
+
+(** This file deal with parsing inline contents. It is called /objects= in =org-element=.
+This is the list of possible elements:
+- emphasis (of several kind : italic, underline, bold, ...)
+- entities : html or latex entities embded in org files, such as \alpha, &aacute, ...
+- export-snippet [\@type\{value\}] : this construction is to insert
+  raw data for a specific exporter:for instance [\@html\{\<p\>Paragraph\</p\>\}]
+- footnote reference : [[fn:foo]] for instance
+- inline babel call : make a call to a babel source block. More details [[http://orgmode.org/manual/Evaluating-code-blocks.html][here]].
+- inline source block : [src_lang[arg]\{contents\}]
+- latex fragment : =$foo$=
+- line break : =\\\\= at the end of a line
+- link : =[[descr][target]]= 
+- macro : text substitution [{{{name(arg1, arg2)}}}]. Documentation [[http://orgmode.org/manual/Macro-replacement.html][here]].
+- radio target : special links
+- statistics-cookie : statistics about the percentage of done children
+- verbatim : [=plaincode=]
+- superscript/subscript : [^\{foo\}] and [_\{foo\}]
+- targets : special links [<<<target>>>]
+- timestamp : scheduled/deadline or plain/inactive timestamp
+
+The file is organized as follows :
+- definition of types corresponding to the category below
+- definition of what a parser is (see below for more information)
+- definition of parsers for each syntaxic categories
+- glue that does the job
+*)
+
+(** {1 Type definitions} *)
+
+(**
+This section follows the following convention :
+- a type for each category
+- all types are mutually recursive, if not needed.
+
+Indeed for some categories (link, emphasis, ...) we need to embded inline content.
+*)
+
+(** {2 Emphasis} *)
+(** org supports three types of emphasis :
+- bold (with stars : [*foo*])
+- italic (with slashes : [/foo/])
+- underline (with underscores : [_foo_])
+*)
+type emphasis = [`Bold | `Italic | `Underline] * t list
+
+(** {2 Entities} *)
+(** Entity are defined in the module {!Entity}. *)
+
+and entity = Entity.t
+
+(** {2 Export snippett} *)
+(** An export snippet is given by a couple [(language, text)].*)
+and export_snippet = string * string
+
+(** {2 The type of inline contents} *)
+(** The final type for {!Inline.t} is as follows: *)
+and t = 
+  | Emphasis of emphasis 
+  | Entity of entity
+  | Export_Snippet of export_snippet
+  | Plain of string
+
+(** {1 Parsers} *)
+(** This section deals with defining what a parser for a construction is. *)
+
+type parser = (string -> t list) -> BatSubstring.t -> (t list * BatSubstring.t) option
+  (** A parser simply a function taking a [BatSubstring.t] and returning an option of
+      [t list]. The parser takes an extra parameter, a function to parse a
+      string and return a list of {!Inline.t}. The function may as well throw an
+      exception.
+  *)
+
+val run_parsers : parser list -> string -> t list
+(** Run a list of parsers over a string and returned the generated contents.
+    Note that the order matter : first parsers have higher priority. *)
+
+val parse : string -> t list
+(** Parse a string *)
+
