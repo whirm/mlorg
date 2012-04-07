83533805960d63dead8a1d29ae500a9e5618b253
Simon Castellan <simon.castellan@iuwt.fr>
2012-04-07 17:19:02 +0200

    Implementation of Block.

---
+++ syntax/blocks/block.ml
@@ -0,0 +1,18 @@
+type heading = {
+  title: Inline.t list;
+  tags: string list;
+  marker: string option; (** TODO, DONE, and so on *)
+  level: int;
+  priority: char option
+}
+and list_item = {
+  contents: t list;
+  number: string option; (** The optional number of the item *)
+  checkbox: bool option; (** Does it have a checkbox ([[ ]]) and is it checked ? *)
+}
+and t = 
+  | Paragraph of Inline.t list
+  | Heading of heading
+  | List of list_item list
+  | Directive of string * string
+
