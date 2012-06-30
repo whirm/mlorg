c6a6b4a08f693bb978871c9fafab1918b24264a1
Simon Castellan <simon.castellan@iuwt.fr>
2012-07-01 00:58:38 +0200

    Redone Html exporter with bottomUp traversal.

--- exports/html.ml
+++ exports/html.ml
@@ -6,7 +6,7 @@ open Block
 open Document
 open Modules
 open Config
-
+open Xml
 module E = struct
   module Meta = struct
     let name = "html"
@@ -14,79 +14,83 @@ module E = struct
     let encoding = Config.add config "encoding" string "The document's encoding" "utf-8"
     let full = Config.add config "wrap" boolean "Shall the output be a full html document ?" true
     let style = Config.add config "style" string "The stylesheet to use" "style.css"
+    let config = Config.validate config
   end
   open Meta
+
   let concatmap f l = List.concat (List.map f l)
   let assoc l s = try List.assoc s l with _ -> ""
-  let export doc out = 
-    let o = object(self)
-      inherit [Xml.t list] Document.folder as super
-      method wrap d contents = 
-        let head = Xml.block "head"
-          [Xml.block "title" [Xml.data (Xml.escape_entities d.title)];
-           Xml.block "meta" ~attr: ["http-equiv", "Content-Type"; "content", "text/html";
-                                   "charset", Config.get encoding] [];
-           Xml.block "script" ~attr:["type","text/javascript"; 
-                                     "src","http://orgmode.org/mathjax/MathJax.js"] [Xml.data " "];
-           Xml.block "link" ~attr: ["rel", "stylesheet"; "href", Config.get style;
+  class htmlExporter { get } = object(self)
+    inherit [Xml.t list] Document.bottomUp as super
+    method bot = []
+    method combine = List.concat
+    method wrap d contents = 
+      let head = Xml.block "head"
+        [Xml.block "title" [Xml.data (Xml.escape_entities d.title)];
+         Xml.block "meta" ~attr: ["http-equiv", "Content-Type"; "content", "text/html";
+                                  "charset", get encoding] [];
+         Xml.block "script" ~attr:["type","text/javascript"; 
+                                   "src","http://orgmode.org/mathjax/MathJax.js"] [Xml.data " "];
+         Xml.block "link" ~attr: ["rel", "stylesheet"; "href", get style;
                                     "type", "text/css"; "media", "screen"] []]
-        in
-        Xml.block "html"
-          ~attr:["xmlns", "http://www.w3.org/1999/xhtml"]
-          [head; Xml.block "body" contents]
-      method inline _ = function
-        | Plain s -> [Xml.data (Xml.escape_entities s)]
-        | Emphasis (kind, data) ->
+      in
+      Xml.block "html"
+        ~attr:["xmlns", "http://www.w3.org/1999/xhtml"]
+        [head; Xml.block "body" contents]
+    method inline = function
+      | Plain s -> [Xml.data (Xml.escape_entities s)]
+      | Emphasis (kind, data) ->
           let l = [`Bold, "b"; `Italic, "i"; `Underline, "u"] in
-          [Xml.block (List.assoc kind l) (self#inlines_ data)]
-        | Entity e -> 
+          [Xml.block (List.assoc kind l) (self#inlines data)]
+      | Entity e -> 
           [Xml.data e.html]
-        | Latex_Fragment (Inline.Math s) -> [Xml.data (Xml.escape_entities ("\\("^s^"\\)"))]
-        | Link {url; label} ->
-            [Xml.block "a" ~attr: ["href", Inline.string_of_url url]
-                (self#inlines_ label)]
-        | Verbatim s -> 
+      | Latex_Fragment (Inline.Math s) -> [Xml.data (Xml.escape_entities ("\\("^s^"\\)"))]
+      | Link {url; label} ->
+          [Xml.block "a" ~attr: ["href", Inline.string_of_url url]
+              (self#inlines label)]
+      | Verbatim s -> 
           [Xml.block "code" [Xml.data (Xml.escape_entities s)]]
-        | x -> super#inline [] x
-      method inlines_ l = 
-        concatmap (self#inline []) l
-      method list_item _ x = 
-        let contents = match x.contents with
-          | [Paragraph i] -> self#inlines_ i
-          | _ -> self#blocks [] x.contents
-        in
-        match x.number with
+      | x -> super#inline x
+    method list_item x = 
+      let contents = match x.contents with
+        | [Paragraph i] -> self#inlines i
+        | _ -> self#blocks x.contents
+      in
+      match x.number with
         | None -> [Xml.block "li" contents]
         | Some number ->
-          [Xml.block ~attr: ["style", "list-style-type: none"] "li"
-            (Xml.data number :: contents)]
-      method block _ = function
-        | Paragraph l -> [Xml.block "p" (self#inlines_ l)]
-        | List (l, _) ->
-          [Xml.block "ul" (concatmap (self#list_item []) l)]
-        | Example (_, l) ->
-            [Xml.block "pre" [Xml.data (String.concat "\n" l)]]
-        | Quote l ->
-            [Xml.block "blockquote" (self#blocks [] l)]
-        | x -> super#block [] x
-      method blocks _ l = 
-        concatmap (self#block []) l
-      method heading _ d = 
-        (Xml.block (Printf.sprintf "h%d" d.level)
-            (self#inlines_ d.name)) ::
-          (self#blocks [] d.content
-           @ concatmap (self#heading []) d.children)
-      method document _ d =
-        [Xml.block "div" ~attr:["id", "content"]
-            (self#blocks [] d.beginning @
-               concatmap (self#heading []) d.headings)]
-    end
-    in 
-    if Config.get full then
-      (Printf.fprintf out "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">";
-       Xml.output out (o#wrap doc (o#document [] doc)))
+            [Xml.block ~attr: ["style", "list-style-type: none"] "li"
+                (Xml.data number :: contents)]
+    method block = function
+      | Paragraph l -> [Xml.block "p" (self#inlines l)]
+      | List (l, _) ->
+          [Xml.block "ul" (concatmap (self#list_item) l)]
+      | Example (_, l) ->
+          [Xml.block "pre" [Xml.data (String.concat "\n" l)]]
+      | Quote l ->
+          [Xml.block "blockquote" (self#blocks l)]
+      | x -> super#block x
+    method heading d = 
+      (Xml.block (Printf.sprintf "h%d" d.level)
+         (self#inlines d.name)) ::
+        (self#blocks d.content
+         @ concatmap (self#heading) d.children)
+    method document d =
+      [Xml.block "div" ~attr:["id", "content"]
+          (self#blocks d.beginning @
+             concatmap (self#heading) d.headings)]
+  end
+
+
+  let doctype = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">"
+
+  let with_custom_exporter o { get } doc out = 
+    if get full then
+      (IO.nwrite out doctype;
+       Xml.output out (o#wrap doc (o#document doc)))
     else
-      List.iter (Xml.output out) (o#document [] doc)
+      List.iter (Xml.output out) (o#document doc)
+  let export c = with_custom_exporter (new htmlExporter c) c
   let default_filename = change_ext "html"
 end
 let _ = Modules.Exporters.add (module E : Exporters.Signature)        
--- main.ml
+++ main.ml
@@ -8,7 +8,7 @@ open Quote
 open Filter
 
 (* pure part *)
-let generate filename backend output = 
+let generate filename backend output opts = 
   let export = Modules.Exporters.find backend in
   try
     let module E = (val export : Modules.Exporters.Signature) in
@@ -27,20 +27,21 @@ let generate filename backend output =
       else 
         Document.from_file filename
     in
-    Modules.Exporters.run backend doc fdout;
+    Modules.Exporters.run backend (Config.parse_comma opts) doc fdout;
     if output = "-" then IO.close_out fdout
 
   with Sys_error msg -> Printf.eprintf "%s\n" msg
     | Not_found -> Printf.eprintf "Backend `%s' does not exist." backend
 let output = ref "" and filename = ref "-"
 let backend = ref "html"
-
+let opts = ref ""
 
 let _ = if not !Sys.interactive then (
   let open Arg in
       parse ["--filename", Set_string filename, "Filename to convert (default: stdin)";
              "--output", Set_string output, "Output file";
+             "--opts", Set_string opts, "Backend options (eg. foo=bar,foo'=bar',..)";
              "--backend", Set_string backend, "Output backend"]
       (fun _ -> ()) "mlorg";
-      generate !filename !backend !output
+      generate !filename !backend !output !opts
 )
