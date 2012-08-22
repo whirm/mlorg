9fda961e230304128314d8983872d64fc7044eff
Simon Castellan <simon.castellan@iuwt.fr>
2012-08-22 11:26:27 +0200

    Main: changed the interface of the mlorg program.

--- main.ml
+++ main.ml
@@ -34,18 +34,39 @@ let output = ref "" and filename = ref "-"
 let backend = ref "html"
 let opts = ref []
 
+let add_opt_i key value = opts := (key, value) :: !opts
+
 let add_opt s = try
   let (a, b) = String.split s "=" in
   opts := (a, b) :: !opts
   with Not_found -> Log.warning "%s: invalid option (should be: var=value)" s
+
+let with_filename s f = 
+  if s = "-" then f stdin
+  else BatFile.with_file_in s f
+let get_directive s = 
+  with_filename !filename
+      (fun fd -> try
+        Parser.parse_lazy (BatIO.lines_of fd) |>
+            Enum.find_map (function
+              | Block.Directive (key, value) when key = s -> Some value
+              | _ -> None) |> print_endline |> fun () -> exit 0
+        with Not_found -> Log.fatal "Directive %s not found." s)
 
 let _ = if not !Sys.interactive then (
   let open Arg in
       parse ["--filename", Set_string filename, "Filename to convert (default: stdin)";
              "--output", Set_string output, "Output file";
              "--set", String add_opt, "Set an option (of the form foo=bar)";
+             "--directive", String get_directive, "Output the contents of the given directive";
+             "--custom", String (fun s ->
+               backend := "quote";
+               if Sys.file_exists s then
+                 add_opt_i "exporters.quote.external-file" s
+               else
+                 add_opt_i "exporters.quote.block" s), "<name> Run a custom exporter on the file (using Quote). <name> can be either a file (containing ML source code) or the name of a source block inside the document";
              "--possible-options", Unit (fun () -> Plugin.eprint_config_descr (); exit 0), "Describe possible options of backends";
               "--backend", Set_string backend, "Output backend"]
-      (fun _ -> ()) "mlorg";
+      (fun s -> filename := s) "mlorg";
       generate !filename !backend !output !opts
 )
