6bb0dc53d52aa268c6a33c6463a62023219da812
Simon Castellan <simon.castellan@iuwt.fr>
2013-05-26 20:19:36 +0200

    Rethought dynamic.

--- plugins/dynamic.ml
+++ plugins/dynamic.ml
@@ -37,10 +37,6 @@ let _ = %s.register (
       T.name number file (String.concat "\n" lines);
       close_out fd
 
-  let load obj = 
-    try Dynlink.loadfile obj
-    with Dynlink.Error e ->
-      Log.fatal "Error while loading %s: %s" obj (Dynlink.error_message e)
   let is_newer f1 f2 = 
     let open Unix in
     try
@@ -51,44 +47,69 @@ let _ = %s.register (
       with _ -> false
     with _ -> true
 
-  let compile ?(clean = true) conf source obj = 
+  let link obj = 
+    try ref := None; Dynlink.loadfile obj; !ref
+    with Dynlink.Error e ->
+      Log.error "Error while loading %s: %s" obj (Dynlink.error_message e);
+      None
+
+
+  let compile ?(clean = true) ~file conf = 
+    let obj = change_ext (if Dynlink.is_native then "cmx" else "cmo") file in
     let command = Printf.sprintf "%s %s -o %s"
       (if Dynlink.is_native then Config.get conf ocamlopt 
        else Config.get conf ocamlc)
-      source
+      file
       obj
     in
     let () = 
-      Log.info "Compiling %s [%s]" source command;
       try
-        Command.run command |> ignore
+        Log.info "Compiling %s [%s]" file command;
+        if is_newer file obj then Command.run command |> ignore
       with Command.Failed (command, error) ->
-        Log.fatal "While compiling (command: %s):\n%s" command error
+        Log.error "While compiling (command: %s):\n%s" command error;
     in
-    let () = load obj in
-    if clean then
-      (Sys.remove source; Sys.remove obj;
-       Sys.remove (change_ext "cmi" obj))
-        
+    let rm f = try Sys.remove f with _ -> () in
+    let v = link obj in
+    if clean then List.iter rm [file; obj; change_ext "cmi" obj; change_ext "o" obj];
+    v
 
+      
     let load ?ml_source conf lines file number = 
       match ml_source with
         | None ->
           File.with_temporary_out ~suffix:".ml" (fun fd source ->
-            let obj = change_ext (if Dynlink.is_native then "cmx" else "cmo") source in
             write_ml_source fd lines file number;
-            compile conf source obj;
-            !ref)
+            compile conf ~file: source)
         | Some source ->
-          let obj = change_ext (if Dynlink.is_native then "cmx" else "cmo") source in
-          if is_newer source obj then
-            File.with_file_out source (fun fd ->
-              write_ml_source fd lines file number;
-              compile ~clean: false conf source obj
-            )
-          else
-              load obj;
-          !ref
+          File.with_file_out source (fun fd -> write_ml_source fd lines file number);
+          compile ~clean: false conf ~file: source
+
+    let load_source config source doc = 
+      let ret ?ml_source file lines number = 
+        load ?ml_source config lines file number in
+      let file source = compile ~clean: false config ~file: source in
+      let block ml_source = function
+        | Block.Src (number, _, lines) -> ret ?ml_source doc.Document.filename lines number
+        | _ -> Log.warning "Denoted block is not a source block."; None
+      in 
+      let lines ml_source code = ret ?ml_source "<command-line>" 
+        (String.nsplit ~by: "\n" code) 1 in
+      try 
+        let (Some b) = Document.find_block_by_name source doc in
+        block None b
+      with _ ->
+        if Sys.file_exists source then file source
+        else
+          try Scanf.sscanf source "%[^:]:%[^:]:%[^\\00]" (fun typ ml_source data ->
+            let ml_source = if ml_source = "" then None else Some ml_source in
+            (match typ with
+            | "block" -> 
+              (try let (Some b) = Document.find_block_by_name data doc in
+                   block ml_source b
+               with _ -> Log.error "Invalid block named %s" data; None)
+            | "ml" -> lines ml_source data))
+          with _ -> lines None source
 end
 module U = Make (struct let name = "Dynamic" type t = unit end)
 include U
--- plugins/dynamic.mli
+++ plugins/dynamic.mli
@@ -3,19 +3,49 @@
 (** This module defines a way for loading dynamic module register values of
     certain type *)
 
-module Make (F : sig val name : string type t  end) : sig
+module Make (F : sig 
+  val name : string 
+  (** Named of the module to register on (should be the name of the
+      module as it is exported) *)
+  type t 
+  (** Type of values to be registered *)
+end) : sig
   val register : F.t -> unit
   (** register a value. To be used only by the generated source code *)
+
+  val link : string -> F.t option
+  (** [link binary_file] dynamically links the file [binary_file],
+      waits for it to register a value and return it if it did, or None
+      if something went wrong. *)
+
+  val compile : ?clean : bool -> file: string -> Config.instance -> F.t option
+  (** [compile ?clean ~file config] compiles ML file [files], {!link}
+      it and returns the value. (The config is needed to know the path to ocaml)
+
+      clean is used to know whether we should remove [file] and the
+      generated files. NB: the name of the generated object file is
+      deduced from the filename. *)
   val load : ?ml_source : string -> Config.instance -> string list -> string -> int -> F.t option
-(** [load config lines filename linenumber] compiles the source code [lines]
-    coming from [filename] and starting at [linenumber], and loads it, returing the
-    registered value. The optional parameter if set may be used not to use a
-    temporary file (It is then the path to the ML file the source code should be written in *)
+(** [load config lines filename linenumber] compiles the source code
+    [lines] coming from [filename] and starting at [linenumber], and
+    loads it, returing the registered value. The optional parameter if
+    set may be used not to use a temporary file (It is then the path
+    to the ML file the source code should be written in *)
+  val load_source : Config.instance -> string -> Document.t -> F.t option
+  (** [load_source config source doc] parses and compile a source code whose form can be:
+      - [block:name]: then it refers to the content of the block [name] in the document
+      - [file:file]: refers the content of file
+      - [inline:source:code]: refers to [code] and specifies that it is to be written in [source] (optional) *)
+      
+
 end
+
 (** This modules given a name and a type will return a generic loader of dynamic
     module.  The name should be the name of the Make(...) module so that
     [name.register] points to the returned register function (used in the source
     during the compilation)
 *)
 val register : unit -> unit
+(** Register a unit value *)
 val load : ?ml_source : string -> Config.instance -> string list -> string -> int -> unit option
+(** Load a unit value *)
