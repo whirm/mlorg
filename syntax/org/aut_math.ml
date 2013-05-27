c8746db2ad3733c122e6a972231acb4abe9b800d
Simon Castellan <simon.castellan@iuwt.fr>
2013-05-27 22:34:39 +0200

    math: displays the line number of an unifinished block math

--- syntax/org/aut_math.ml
+++ syntax/org/aut_math.ml
@@ -3,11 +3,11 @@
 open Batteries
 open Block
 open Org_automaton
-type state = string list * bool
-(* The state : the lines seen so far, and are we done *)
+type state = (string list * bool) * int
+(* The state : the lines seen so far, are we done, and the line number *)
 
-let interrupt context (lines, b) _ = 
-  if not b then Log.warning "$$ not terminated";
+let interrupt context ((lines, b), numb) _ = 
+  if not b then Log.warning "$$ not terminated (started at line %d)" numb;
   context, [Math (String.concat "\n" (List.rev lines))]
         
 let handle_line (lines, _) line =
@@ -17,17 +17,17 @@ let handle_line (lines, _) line =
   else
     line :: lines, false
 
-let parse_line (lines, b) { line; parse; context } = 
+let parse_line ((lines, b), num) { line; parse; context } = 
   if b then 
-    let context, blocks = interrupt context (lines, b) () in
+    let context, blocks = interrupt context ((lines, b), num) () in
     context, Done (blocks, false)
   else
-    context, Next (handle_line (lines, b) line)
+    context, Next (handle_line (lines, b) line, num)
 
 
-let is_start { line; context } = 
+let is_start { line; context; } = 
   try Scanf.sscanf line " $$%[^\n]"
-        (fun l -> Some (context, handle_line ([], false) l))
+        (fun l -> Some (context, (handle_line ([], false) l, context.Org_context.number)))
   with _ -> None
 
 let priority = 10
